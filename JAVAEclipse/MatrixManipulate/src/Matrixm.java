39e0412602de189c8b16604357a5f91419812ce6
keshawntriplett <k908t147@ku.edu>
2017-03-26 12:28:58 -0500

    Sub Matrix Function addition.

--- JAVAEclipse/MatrixManipulate/src/Matrixm.java
+++ JAVAEclipse/MatrixManipulate/src/Matrixm.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.Scanner;
 import java.io.*;
 public class Matrixm {
 	private int N;             // number of rows
@@ -11,4 +10,39 @@ public class Matrixm {
 	  N=n;
 	  
   }
+  public Matrixm subMatrixm(int r, int c)
+  {
+  	Matrixm sub= new Matrixm(N-1);
+  	double[][] m = new double[N-1][];
+      for(int k=0;k<(N-1);k++)
+      {
+          m[k] = new double[N-1];
+      }
+   
+      int row = 0; 
+  	for (int i = 0; i < N; ++i)
+  	{
+  		if (i == r) continue;
+  		
+  		int col = 0;
+  		for (int j = 0; j < N; ++j)
+  		{
+  			if (j == c) continue;
+  			
+  			m[row][col] = data[i][j];
+  			++col;
+  		}
+  		
+  		++row;
+  	}
+
+      
+  	sub.data=m;
+  	
+  	return sub;
+  	
+  }
+  
+  
+  
 }
