0dd83d149d6cf5a13d5caca46736d67411319991
keshawntriplett <k908t147@ku.edu>
2017-03-26 12:18:48 -0500

    Initial Commit W/ class constructor

---
+++ JAVAEclipse/MatrixManipulate/src/Matrixm.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+import java.io.*;
+public class Matrixm {
+	private int N;             // number of rows
+	// number of columns
+     double[][] data;   // M-by-N array
+	
+  public Matrixm(int n)
+  {
+	  N=n;
+	  
+  }
+}
